c2f74b216fe241af0a4ca083a497e259dd5588a4
Antonio Gallegos <antoniogallegos086@gmail.com>
2021-08-31 22:11:31 -0500

    Full data

--- app/src/main/java/com/gama/k/MainActivity.kt
+++ app/src/main/java/com/gama/k/MainActivity.kt
@@ -3,17 +3,35 @@ package com.gama.k
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.renderscript.ScriptGroup
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import com.gama.k.databinding.ActivityMainBinding
+import java.text.FieldPosition
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
     private lateinit var mBinding : ActivityMainBinding
+    private lateinit var aaCountries : ArrayAdapter<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
 
-        val aaCountries = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item)
+        aaCountries = ArrayAdapter<String>(this, R.layout.support_simple_spinner_dropdown_item)
+        aaCountries.addAll(Arrays.asList(" ","México","Bulgaria","Alemania","USA","Brasil"))
+        aaCountries.add("Rusia")
+        mBinding.spinnerCountries.onItemSelectedListener = this
         mBinding.spinnerCountries.adapter = aaCountries
+    }
+
+    override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position : Int, p3: Long) {
+        val myCountry = aaCountries.getItem(position)
+        mBinding.tvSelected.text = "You select $myCountry as your country"
+    }
+
+    override fun onNothingSelected(p0: AdapterView<*>?) {
+
     }
 }
